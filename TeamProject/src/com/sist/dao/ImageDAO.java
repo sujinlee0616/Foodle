8bf6adab7eda18a67451758f45d78dcfe3958fc3
user194 <user194@DESKTOP-ULGK6NE>
2020-03-23 17:54:39 +0900

    200323 create imagedao & change imagemanager

---
+++ TeamProject/src/com/sist/dao/ImageDAO.java
@@ -0,0 +1,82 @@
+package com.sist.dao;
+import java.util.*;
+import java.sql.*;
+import com.sist.vo.*;
+public class ImageDAO {
+	
+	private Connection conn; // Socket
+	private PreparedStatement ps;// OutputStream , BufferedReader
+	private final String URL="jdbc:oracle:thin:@211.238.142.207:1521:XE";
+	//private final String URL="jdbc:oracle:thin:@localhost:1521:XE";
+			
+
+	// 드라이버 등록 => 한번만 수행
+	public ImageDAO()
+     {
+    	 try
+    	 {
+    		 Class.forName("oracle.jdbc.driver.OracleDriver");
+    	 }catch(Exception ex) 
+    	 {
+    		 ex.printStackTrace();
+    	 }
+     }
+
+	// 오라클 연결
+	public void getConnection() {
+		try {
+			conn = DriverManager.getConnection(URL, "hr", "happy");
+		} catch (Exception ex) {
+		}
+	}
+
+	// 오라클 해제
+	public void disConnection() {
+		try {
+			if (ps != null)
+				ps.close();
+			if (conn != null)
+				conn.close();
+		} catch (Exception ex) {
+		}
+	}
+	// 오라클에 이미지 테이블 생성하기!
+	public void CreateImageTable()
+	{
+		try {
+			getConnection();
+			String sql="CREATE TABLE image("
+					+ "rno NUMBER, "
+					+ "iname VARCHAR2(200) CONSTRAINT image_iname_nn NOT NULL, "
+					+ "ilink VARCHAR2(2000) CONSTRAINT image_ilink_nn NOT NULL, "
+					+ "CONSTRAINT image_rno_fk FOREIGN KEY(rno) "
+					+ "REFERENCES maininfo(rno));";
+			ps=conn.prepareStatement(sql);
+			ps.executeQuery();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			disConnection();
+		}
+	}
+	
+	// 이미지 오라클에 넣기!
+	public void InsertImageData(ImageVO vo)
+	{
+		try {
+			getConnection();
+			String sql="INSERT INTO image VALUES("
+					+ "?,?,?)";
+			ps=conn.prepareStatement(sql);
+			ps.setInt(1, vo.getR_No());
+			ps.setString(2, vo.getI_Name());
+			ps.setString(3, vo.getI_Link());
+			ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			disConnection();
+		}
+	}
+}
--- TeamProject/src/com/sist/manager/ImageManager.java
+++ TeamProject/src/com/sist/manager/ImageManager.java
@@ -1,6 +1,8 @@
 package com.sist.manager;
 
 import java.util.*;
+
+import com.sist.dao.ImageDAO;
 import com.sist.vo.*;
 
 import org.jsoup.Jsoup;
@@ -8,17 +10,16 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-
 public class ImageManager {
-
+	ImageDAO dao=new ImageDAO();
 	public ArrayList<ImageVO> ImageAllData(ArrayList<AreacodeVO> areacode) {
 		
 		ArrayList<ImageVO> list = new  ArrayList<ImageVO>();
 		ImageVO vo;
 		
 		ArrayList<AreacodeVO> ac = areacode;
-		int page = 1;
-		int kategorie = 3;
+		int page = 30;
+		int kategorie = 25;
 		
 		Element image;
 		Element image_Name;
@@ -39,28 +40,34 @@ public class ImageManager {
 						Element elem = link.get(z);
 						String mLink = "http://www.menupan.com" + elem.attr("href");
 						Document doc2 = Jsoup.connect(mLink).get();
-					
+						
 						//이미지 파싱
 						int count=0;
 						while (true) {
 							try {
-								
-								vo = new ImageVO(); 
+								System.out.println(count);
+								vo = new ImageVO();
+								System.out.println("========vo.no 시작========");
 								//가게No넣는부분인데 겹치지 않게 하기 위해서
-								vo.setR_No(((z) + ((j)*kategorie) + ((i)*(page*kategorie))));							
-								
+								vo.setR_No(((z) + ((j)*kategorie) + ((i)*(page*kategorie))));	
+								System.out.println("========vo.no 끝========");
 								//사진링크 넣는 부분
+								System.out.println("======이미지링크 시작=======");
 								image = doc2.select("img#restphoto_img_" + count).get(0);
-								vo.setI_Link("https://www.menupan.com/" + image.attr("src"));
-
-
+								vo.setI_Link("https://www.menupan.com" + image.attr("tag_src_z"));
+								System.out.println(vo.getI_Link());
+								System.out.println("======이미지링크 끝=======");
+								
 								//사진 이름 넣는 부분
+								System.out.println("=======이미지 이름 시작=========");
 								image_Name = doc2.select("img#restphoto_img_"+count).get(0);
 								vo.setI_Name(image_Name.attr("title"));
+								System.out.println(vo.getI_Name());
+								System.out.println("=======이미지 이름 끝=========");
 								
-								list.add(vo);
+								dao.InsertImageData(vo);
 								count++;
-						
+								Thread.sleep(1000);
 							} catch (Exception ex) 
 							{
 								ex.printStackTrace();
@@ -80,16 +87,19 @@ public class ImageManager {
 		ImageManager im = new ImageManager();
 		AreacodeManager am = new AreacodeManager();
 		
-		ArrayList<ImageVO> list = new  ArrayList<ImageVO>();
-		
-		list = im.ImageAllData(am.AreacodeAllData());
-		
-		for(int i = 0; i < list.size() ; i++)
-		{
-			System.out.println(list.get(i).getR_No());
-			System.out.println(list.get(i).getI_Name());
-			System.out.println(list.get(i).getI_Link());
-		}
+		im.ImageAllData(am.AreacodeAllData());
+		System.out.println("===================parsing done===================");
+		System.out.println("===================parsing done===================");
+//		ArrayList<ImageVO> list = new  ArrayList<ImageVO>();
+//		
+//		list = im.ImageAllData(am.AreacodeAllData());
+//		System.out.println("======================메인부분=======================");
+//		for(int i = 0; i < list.size() ; i++)
+//		{
+//			System.out.println(list.get(i).getR_No());
+//			System.out.println(list.get(i).getI_Name());
+//			System.out.println(list.get(i).getI_Link());
+//		}
 	}
 	
 }
