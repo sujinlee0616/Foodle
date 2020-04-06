850ada29b07ae0388090cc9fc66cc0498a50baeb
Sujin Lee <sjnu21@naver.com>
2020-04-06 17:48:53 +0900

    메인 주간Top30 데이터 연동

---
+++ TeamProject/src/com/sist/dao/CreateSQLSessionFactory.java
@@ -0,0 +1,28 @@
+// 모든 DAO에 공통적으로 들어가는 소스 따로 뺐음.  
+package com.sist.dao;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.*;
+
+public class CreateSQLSessionFactory {
+	private static SqlSessionFactory ssf;
+	static
+	{
+		try
+		{
+			// Config.xml 로딩 
+			Reader reader = Resources.getResourceAsReader("Config.xml");
+			ssf=new SqlSessionFactoryBuilder().build(reader);
+		}catch (Exception ex) {
+			ex.getMessage();
+		}
+	}
+
+	public static SqlSessionFactory getSsf() {
+		return ssf;
+	}
+	
+}
---
+++ TeamProject/src/com/sist/dao/MainDAO.java
@@ -0,0 +1,37 @@
+package com.sist.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import com.sist.vo.MainInfoVO;
+
+public class MainDAO {
+	private static SqlSessionFactory ssf;
+	static
+	{
+		ssf=CreateSQLSessionFactory.getSsf();
+	}
+	
+	// [mainImageData] - MyBatis 에러 잡기 쉽게 try~catch 절로 만들었음 
+		public static List<MainInfoVO> weeklyTop30()
+		{
+			SqlSession session=null; // null 해줘야!
+			List<MainInfoVO> list=new ArrayList<MainInfoVO>();
+			
+			try {
+				session=ssf.openSession(); // 이전에 코딩했던 버젼의 getConnection에 해당함 
+				list=session.selectList("weeklyTop30"); // 이전에 코딩했던 버젼의, preparedStautement에 SQL문 갖다놓는 것에 해당함
+				// SQL문: main-mapper.xml의 id="weeklyTop30"인 SQL문.
+			} catch (Exception ex) {
+				System.out.println("weeklyTop30(): "+ex.getMessage());
+			}
+			finally {
+				if(session!=null)
+					session.close();
+			}
+			return list;
+		}
+		
+}
