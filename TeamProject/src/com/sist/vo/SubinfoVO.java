516155bb12ead7f08c0f3ee9064175a146091999
pch9501 <pcj9501@naver.com>
2020-04-06 18:10:47 +0900

    Subinfo 생성

---
+++ TeamProject/src/com/sist/vo/SubinfoVO.java
@@ -0,0 +1,88 @@
+package com.sist.vo;
+import java.util.*;
+
+public class SubinfoVO {
+	private int rNo,rGood,rHit;
+	private String rDrink;
+	private String rNosmoking;
+	private String rPark;
+	private String rDelivery;
+	private String rOther;
+	private String rTakeout;
+	private String rContent;
+	private String rStart,rDate;
+	public int getrNo() {
+		return rNo;
+	}
+	public void setrNo(int rNo) {
+		this.rNo = rNo;
+	}
+	public int getrGood() {
+		return rGood;
+	}
+	public void setrGood(int rGood) {
+		this.rGood = rGood;
+	}
+	public int getrHit() {
+		return rHit;
+	}
+	public void setrHit(int rHit) {
+		this.rHit = rHit;
+	}
+	public String getrDrink() {
+		return rDrink;
+	}
+	public void setrDrink(String rDrink) {
+		this.rDrink = rDrink;
+	}
+	public String getrNosmoking() {
+		return rNosmoking;
+	}
+	public void setrNosmoking(String rNosmoking) {
+		this.rNosmoking = rNosmoking;
+	}
+	public String getrPark() {
+		return rPark;
+	}
+	public void setrPark(String rPark) {
+		this.rPark = rPark;
+	}
+	public String getrDelivery() {
+		return rDelivery;
+	}
+	public void setrDelivery(String rDelivery) {
+		this.rDelivery = rDelivery;
+	}
+	public String getrOther() {
+		return rOther;
+	}
+	public void setrOther(String rOther) {
+		this.rOther = rOther;
+	}
+	public String getrTakeout() {
+		return rTakeout;
+	}
+	public void setrTakeout(String rTakeout) {
+		this.rTakeout = rTakeout;
+	}
+	public String getrContent() {
+		return rContent;
+	}
+	public void setrContent(String rContent) {
+		this.rContent = rContent;
+	}
+	public String getrStart() {
+		return rStart;
+	}
+	public void setrStart(String rStart) {
+		this.rStart = rStart;
+	}
+	public String getrDate() {
+		return rDate;
+	}
+	public void setrDate(String rDate) {
+		this.rDate = rDate;
+	}
+	
+	
+}
