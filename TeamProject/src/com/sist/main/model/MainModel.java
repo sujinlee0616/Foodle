ec2685cf1dfdaafb2b695e610f23f4785ab241e0
hajieun1218 <gkwldms1218@naver.com>
2020-04-13 18:28:28 +0900

    main화면 인기맛집 Top3 데이터 연동

--- TeamProject/src/com/sist/main/model/MainModel.java
+++ TeamProject/src/com/sist/main/model/MainModel.java
@@ -7,19 +7,35 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.sist.controller.Controller;
 import com.sist.controller.RequestMapping;
-import com.sist.service.dao.MainDAO;
-import com.sist.vo.MainInfoVO;
+import com.sist.service.dao.*;
+import com.sist.vo.*;
 
 @Controller
 public class MainModel {
 	@RequestMapping("main/main.do")
 	public String main_main(HttpServletRequest request, HttpServletResponse response)
 	{
-		
-		
 		List<MainInfoVO> weeklytop30list=MainDAO.weeklyTop30();
+		List<MainInfoVO> realTimeTop3list=MainDAO.realTimeTop3();
+		
+		for(MainInfoVO vo:weeklytop30list) {
+			String addr=vo.getrAddr2();
+			if(addr.length()>22) {
+				addr=addr.substring(0,22).concat("...");
+				vo.setrAddr2(addr);
+			}
+		}
+		for(MainInfoVO vo:realTimeTop3list) {
+			String addr=vo.getrAddr2();
+			if(addr.length()>22) {
+				addr=addr.substring(0,22).concat("...");
+				vo.setrAddr2(addr);
+			}
+		}
+		
 		
 		request.setAttribute("weeklytop30list", weeklytop30list);
+		request.setAttribute("realTimeTop3list", realTimeTop3list);
 		request.setAttribute("main_header", "../common/header_main.jsp");
 		request.setAttribute("main_jsp", "../main/home.jsp");
 		
