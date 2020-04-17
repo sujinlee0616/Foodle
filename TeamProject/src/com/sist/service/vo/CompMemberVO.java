dfadda892dd3ac6164892761fde473730f5dacd0
Sujin Lee <sjnu21@naver.com>
2020-04-17 15:28:08 +0900

    기업회원가입 구현완료

--- TeamProject/src/com/sist/service/vo/CompMemberVO.java
+++ TeamProject/src/com/sist/service/vo/CompMemberVO.java
@@ -2,99 +2,99 @@ package com.sist.service.vo;
 
 public class CompMemberVO {
 	
-	private String rId;
-	private String rPwd;
-	private int rNo;
-	private String rName;
-	private String rTel;
-	private String rPost;
-	private String rAddr1;
-	private String rAddr2;
-	private String rType;
-	private String rArea;
-	private String rAreaDetail;
-	private String uType;
-	private String signupDate;
+	private String rid;
+	private String rpwd;
+	private int rno;
+	private String rname;
+	private String rtel;
+	private String rpost;
+	private String raddr1;
+	private String raddr2;
+	private String rtype;
+	private String rarea;
+	private String rareadetail;
+	private String utype;
+	private String signupdate;
+	public String getRid() {
+		return rid;
+	}
+	public void setRid(String rid) {
+		this.rid = rid;
+	}
+	public String getRpwd() {
+		return rpwd;
+	}
+	public void setRpwd(String rpwd) {
+		this.rpwd = rpwd;
+	}
+	public int getRno() {
+		return rno;
+	}
+	public void setRno(int rno) {
+		this.rno = rno;
+	}
+	public String getRname() {
+		return rname;
+	}
+	public void setRname(String rname) {
+		this.rname = rname;
+	}
+	public String getRtel() {
+		return rtel;
+	}
+	public void setRtel(String rtel) {
+		this.rtel = rtel;
+	}
+	public String getRpost() {
+		return rpost;
+	}
+	public void setRpost(String rpost) {
+		this.rpost = rpost;
+	}
+	public String getRaddr1() {
+		return raddr1;
+	}
+	public void setRaddr1(String raddr1) {
+		this.raddr1 = raddr1;
+	}
+	public String getRaddr2() {
+		return raddr2;
+	}
+	public void setRaddr2(String raddr2) {
+		this.raddr2 = raddr2;
+	}
+	public String getRtype() {
+		return rtype;
+	}
+	public void setRtype(String rtype) {
+		this.rtype = rtype;
+	}
+	public String getRarea() {
+		return rarea;
+	}
+	public void setRarea(String rarea) {
+		this.rarea = rarea;
+	}
+	public String getRareadetail() {
+		return rareadetail;
+	}
+	public void setRareadetail(String rareadetail) {
+		this.rareadetail = rareadetail;
+	}
+
+	public String getUtype() {
+		return utype;
+	}
+	public void setUtype(String utype) {
+		this.utype = utype;
+	}
+	public String getSignupdate() {
+		return signupdate;
+	}
+	public void setSignupdate(String signupdate) {
+		this.signupdate = signupdate;
+	}
 	
-	public String getrId() {
-		return rId;
-	}
-	public void setrId(String rId) {
-		this.rId = rId;
-	}
-	public String getrPwd() {
-		return rPwd;
-	}
-	public void setrPwd(String rPwd) {
-		this.rPwd = rPwd;
-	}
-	public int getrNo() {
-		return rNo;
-	}
-	public void setrNo(int rNo) {
-		this.rNo = rNo;
-	}
-	public String getrName() {
-		return rName;
-	}
-	public void setrName(String rName) {
-		this.rName = rName;
-	}
-	public String getrTel() {
-		return rTel;
-	}
-	public void setrTel(String rTel) {
-		this.rTel = rTel;
-	}
-	public String getrPost() {
-		return rPost;
-	}
-	public void setrPost(String rPost) {
-		this.rPost = rPost;
-	}
-	public String getrAddr1() {
-		return rAddr1;
-	}
-	public void setrAddr1(String rAddr1) {
-		this.rAddr1 = rAddr1;
-	}
-	public String getrAddr2() {
-		return rAddr2;
-	}
-	public void setrAddr2(String rAddr2) {
-		this.rAddr2 = rAddr2;
-	}
-	
-	public String getrType() {
-		return rType;
-	}
-	public void setrType(String rType) {
-		this.rType = rType;
-	}
-	public String getrArea() {
-		return rArea;
-	}
-	public void setrArea(String rArea) {
-		this.rArea = rArea;
-	}
-	public String getrAreaDetail() {
-		return rAreaDetail;
-	}
-	public void setrAreaDetail(String rAreaDetail) {
-		this.rAreaDetail = rAreaDetail;
-	}
-	public String getuType() {
-		return uType;
-	}
-	public void setuType(String uType) {
-		this.uType = uType;
-	}
-	public String getSignupDate() {
-		return signupDate;
-	}
-	public void setSignupDate(String signupDate) {
-		this.signupDate = signupDate;
-	}
 	
 	
 
